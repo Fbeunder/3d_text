97eb4eaa7368eccfb574e740d91bf75c37269ff1
Fbeunder <frank@lynxx.eu>
2025-02-26 15:34:17 +0100

    Update 3d_renderer.js voor geavanceerde 3D tekstopties

--- static/js/3d_renderer.js
+++ static/js/3d_renderer.js
@@ -3,6 +3,8 @@
 // Globale variabelen voor Three.js componenten
 let scene, camera, renderer;
 let textMesh;
+let fontLoader;
+let fonts = {};
 
 // Initialiseer de 3D scene
 function initScene() {
@@ -39,8 +41,8 @@ function initScene() {
     directionalLight.position.set(0, 1, 1);
     scene.add(directionalLight);
     
-    // Render de scene
-    renderer.render(scene, camera);
+    // Initialiseer de font loader
+    fontLoader = new THREE.FontLoader();
     
     // Maak de scene responsive
     window.addEventListener('resize', () => {
@@ -49,7 +51,42 @@ function initScene() {
         renderer.setSize(window.innerWidth * 0.8, window.innerHeight * 0.6);
     });
     
+    // Render de scene
+    renderer.render(scene, camera);
+    
     return scene;
+}
+
+// Laad een lettertype en cache het
+function loadFont(fontName) {
+    return new Promise((resolve, reject) => {
+        // Als het lettertype al in cache zit, gebruik het dan
+        if (fonts[fontName]) {
+            resolve(fonts[fontName]);
+            return;
+        }
+        
+        // Anders laden we het lettertype via de CDN
+        // Mapping van lettertypewaarden naar URL's
+        const fontUrls = {
+            'helvetiker': 'https://threejs.org/examples/fonts/helvetiker_regular.typeface.json',
+            'optimer': 'https://threejs.org/examples/fonts/optimer_regular.typeface.json',
+            'gentilis': 'https://threejs.org/examples/fonts/gentilis_regular.typeface.json',
+            'droid': 'https://threejs.org/examples/fonts/droid/droid_sans_regular.typeface.json'
+        };
+        
+        // Controleer of we een URL hebben voor het gevraagde lettertype
+        const fontUrl = fontUrls[fontName] || fontUrls['helvetiker'];
+        
+        fontLoader.load(fontUrl, function(font) {
+            // Cache het lettertype voor later gebruik
+            fonts[fontName] = font;
+            resolve(font);
+        }, undefined, function(error) {
+            console.error('Font loading error:', error);
+            reject(error);
+        });
+    });
 }
 
 // Render de 3D text met de gegeven data
@@ -64,45 +101,52 @@ function renderText(textData) {
         scene.remove(textMesh);
     }
     
-    // Laad de font en maak de 3D tekst
-    const loader = new THREE.FontLoader();
-    loader.load('https://threejs.org/examples/fonts/helvetiker_regular.typeface.json', function(font) {
-        // Maak geometry voor 3D tekst
-        const textGeometry = new THREE.TextGeometry(textData.text, {
-            font: font,
-            size: textData.size,
-            height: textData.height,
-            curveSegments: textData.curveSegments,
-            bevelEnabled: textData.bevelEnabled,
-            bevelThickness: textData.bevelThickness,
-            bevelSize: textData.bevelSize,
-            bevelOffset: textData.bevelOffset,
-            bevelSegments: textData.bevelSegments
+    // Laad het gewenste lettertype
+    loadFont(textData.font)
+        .then(font => {
+            // Maak geometry voor 3D tekst
+            const textGeometry = new THREE.TextGeometry(textData.text, {
+                font: font,
+                size: textData.size,
+                height: textData.height,
+                curveSegments: textData.curveSegments,
+                bevelEnabled: textData.bevelEnabled,
+                bevelThickness: textData.bevelThickness,
+                bevelSize: textData.bevelSize,
+                bevelOffset: textData.bevelOffset,
+                bevelSegments: textData.bevelSegments
+            });
+            
+            // Centreer de tekst
+            textGeometry.computeBoundingBox();
+            const centerOffset = -0.5 * (textGeometry.boundingBox.max.x - textGeometry.boundingBox.min.x);
+            
+            // Maak een materiaal voor de tekst met de gekozen kleur
+            const textMaterial = new THREE.MeshPhongMaterial({
+                color: parseInt(textData.color, 16),
+                specular: 0x111111,
+                shininess: 30
+            });
+            
+            // Maak de mesh met geometry en materiaal
+            textMesh = new THREE.Mesh(textGeometry, textMaterial);
+            textMesh.position.x = centerOffset;
+            
+            // Gebruik positie uit textData als die is opgegeven, anders standaardwaarden
+            if (textData.position) {
+                if (textData.position.y !== undefined) textMesh.position.y = textData.position.y;
+                if (textData.position.z !== undefined) textMesh.position.z = textData.position.z;
+            }
+            
+            // Voeg de mesh toe aan de scene
+            scene.add(textMesh);
+            
+            // Start de animatie
+            animateRotation();
+        })
+        .catch(error => {
+            console.error('Error rendering text:', error);
         });
-        
-        // Centreer de tekst
-        textGeometry.computeBoundingBox();
-        const centerOffset = -0.5 * (textGeometry.boundingBox.max.x - textGeometry.boundingBox.min.x);
-        
-        // Maak een materiaal voor de tekst
-        const textMaterial = new THREE.MeshPhongMaterial({
-            color: 0x156289,
-            specular: 0x156289,
-            shininess: 30
-        });
-        
-        // Maak de mesh met geometry en materiaal
-        textMesh = new THREE.Mesh(textGeometry, textMaterial);
-        textMesh.position.x = centerOffset;
-        textMesh.position.y = 0;
-        textMesh.position.z = 0;
-        
-        // Voeg de mesh toe aan de scene
-        scene.add(textMesh);
-        
-        // Start de animatie
-        animateRotation();
-    });
 }
 
 // Animeer de rotatie van het 3D model
